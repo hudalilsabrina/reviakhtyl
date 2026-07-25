b282134bc9e2798f64e0b9432fcf87606c0a0f05
hudalilsabrina <ariknata308@ gmail.com>
2026-07-25 13:17:29 +0000

    Add API endpoints and email notifications for account suspension

---
+++ app/Http/Controllers/Api/Application/Users/UserSuspensionController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api\Application\Users;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Transformers\Api\Application\UserTransformer;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\AccountSuspended;
+use App\Notifications\AccountUnsuspended;
+
+class UserSuspensionController extends Controller
+{
+    /**
+     * Suspend a user account.
+     */
+    public function suspend(Request $request, User $user): JsonResponse
+    {
+        $data = $request->validate([
+            'reason' => 'required|string',
+            'suspend_until' => 'nullable|date|after:now',
+        ]);
+
+        $user->update([
+            'suspended' => true,
+            'suspended_at' => now(),
+            'suspension_reason' => $data['reason'],
+            'suspend_until' => $data['suspend_until'] ?? null,
+        ]);
+
+        // Suspend all user's servers
+        $user->servers()->update(['status' => 'suspended']);
+
+        // Send notification
+        $user->notify(new AccountSuspended($data['reason'], $data['suspend_until'] ?? null));
+
+        return new JsonResponse([
+            'object' => 'user',
+            'attributes' => (new UserTransformer())->transform($user),
+        ]);
+    }
+
+    /**
+     * Unsuspend a user account.
+     */
+    public function unsuspend(User $user): JsonResponse
+    {
+        $user->update([
+            'suspended' => false,
+            'suspended_at' => null,
+            'suspension_reason' => null,
+            'suspend_until' => null,
+        ]);
+
+        // Unsuspend all user's servers that were suspended
+        $user->servers()->where('status', 'suspended')->update(['status' => null]);
+
+        // Send notification
+        $user->notify(new AccountUnsuspended());
+
+        return new JsonResponse([
+            'object' => 'user',
+            'attributes' => (new UserTransformer())->transform($user),
+        ]);
+    }
+}
---
+++ app/Notifications/AccountSuspended.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class AccountSuspended extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        public string $reason,
+        public ?\DateTimeInterface $suspendUntil = null
+    ) {}
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $message = (new MailMessage)
+            ->error()
+            ->subject('Your Account Has Been Suspended')
+            ->greeting('Account Suspended')
+            ->line('Your account has been suspended by an administrator.')
+            ->line('Reason: ' . $this->reason);
+
+        if ($this->suspendUntil) {
+            $message->line('Suspension expires: ' . $this->suspendUntil->format('F j, Y \a\t g:i A'));
+        } else {
+            $message->line('This is a permanent suspension.');
+        }
+
+        $message->line('If you believe this is a mistake, please contact support.');
+
+        return $message;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'reason' => $this->reason,
+            'suspend_until' => $this->suspendUntil?->format('Y-m-d\TH:i:sP'),
+        ];
+    }
+}
